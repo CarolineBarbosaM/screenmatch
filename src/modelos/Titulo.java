1c0febb4c654a607b6643dcefe6e2b7968cb3127
Caroline Martins <53910116+CarolineBarbosaM@users.noreply.github.com>
2024-05-13 22:36:02 -0300

    :sparkles: trabalhando com consumo de api

--- src/modelos/Titulo.java
+++ src/modelos/Titulo.java
@@ -1,7 +1,13 @@
 package modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+
+    @SerializedName("Title")
     private String nome;
+
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -11,6 +17,12 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb tituloOmdb) {
+        this.nome = tituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(tituloOmdb.year());
+        this.duracaoEmMinuto = Integer.valueOf(tituloOmdb.runtime().substring(0, 2));
     }
 
     public String getNome() {
@@ -66,5 +78,14 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "Titulo{" +
+                "nome='" + nome + '\'' +
+                ", anoDeLancamento=" + anoDeLancamento +
+                ", duracaoEmMinutos=" + duracaoEmMinuto +
+                '}';
     }
 }
